d3ade69bb49ccb5239c2a49c849121d26e5de01e
Willa Walthall <ghthor@gmail.com>
2015-01-23 04:13:07 -0500

    The interfaces for world entities

---
+++ rpg2d/entity/entity.go
@@ -0,0 +1,33 @@
+package entity
+
+import "github.com/ghthor/engine/coord"
+
+// A basic entity in the world.
+type Entity interface {
+	// Unique ID
+	Id() int64
+
+	// Location in the world
+	Cell() coord.Cell
+}
+
+// Represents the current motion state of an
+// entity that can move around in the world.
+type MotionState struct{}
+
+// An entity that has the ability to move
+// through the world.
+type Movable interface {
+	Entity
+
+	// Returns the entities current motion state
+	MotionState() MotionState
+}
+
+// An entity that can collide with other entities.
+type Collidable interface {
+	Entity
+
+	CanCollideWith(other Collidable) bool
+	HadCollisionWith(other Collidable)
+}
