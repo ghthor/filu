372d2ac737bcb800c1f1a40f7e0dd58c86f7222b
Willa Walthall <ghthor@gmail.com>
2015-01-23 04:14:03 -0500

    The skeleton specification for a quad tree

---
+++ rpg2d/quad/quad_test.go
@@ -0,0 +1,24 @@
+package quad_test
+
+import "github.com/ghthor/gospec"
+
+func DescribeQuad(c gospec.Context) {
+	c.Specify("a quad tree", func() {
+		c.Specify("can insert an entity", func() {
+		})
+
+		c.Specify("will subdivide", func() {
+		})
+
+		c.Specify("can remove an entity", func() {
+		})
+
+		c.Specify("can be queried", func() {
+			c.Specify("by cell", func() {
+			})
+
+			c.Specify("by bounds", func() {
+			})
+		})
+	})
+}
