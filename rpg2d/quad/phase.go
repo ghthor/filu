79c03763f8163a133281c180c032ba8e7cc097d0
Willa Walthall <ghthor@gmail.com>
2015-01-25 06:59:58 -0500

    Updated documentation of the 3 simulation phases

--- rpg2d/quad/phase.go
+++ rpg2d/quad/phase.go
@@ -1,27 +1,40 @@
 package quad
 
-// The movement phase is the naive phase that takes
-// the user input and applies it to the entities movement
-// state. This enables the broad phase estimate where
-// collisions might take place if the movement is valid.
+// 1. Input Application Phase - User Defined
+//
+// The input phase takes the user input and applies
+// it to the entities movement state. This enables the
+// broad phase to estimate where collisions might take
+// place if the movement is valid.
 // The narrow phase should revert any changes to the movement
 // state that cannot happen because of collisions.
-type MovementPhaseHandler interface {
-	MovementPhaseIn(Chunk) Chunk
+type InputPhaseHandler interface {
+	InputPhaseIn(Chunk) Chunk
 }
 
-// The broad phase is mostly handled internally.
-// But this handler allows pkg user's to hook into
-// the broad phase. The broad phase is what discovers
-// chunks of interest.
-type BroadPhaseHandler interface {
-	BroadPhaseIn(Chunk) Chunk
-}
+// 2. Broad Phase - Internal
+//
+// The broad phase is only concerned with the bounds
+// of all the entities potential futures. For this
+// reason it will be and internal implementation.
+// The broad phase will need to happen while recursively
+// descending and ascending through the tree. It will be
+// where chunks of interest are created based on the
+// potential future overlapping bounds of the entities.
+// The chunks of interest will then be passed into the
+// user supplied narrow phase handler.
 
-// A collisionhandler takes a chunk of entities
-// and does collision checks for the chunk and modifies
-// the entities. All entities in the returned chunk will
-// be reinserted into the quad tree to update their location.
+// 3. Narrow Phase - User Defined
+//
+// The narrow phase resolves any collisions that the
+// broad phase identified by the bounds of the potential
+// future state of the entity. The broad phase has grouped
+// this chunk together, such that, there is no possible way
+// any of these entities can interact with any entities that
+// are not included in this chunk. The narrow phase should
+// return a chunk.... Honestly I don't know what it should
+// return and I need to stop thinking about it for now and
+// just shut the fuck up and write the fucking code.
 type NarrowPhaseHandler interface {
 	NarrowPhaseIn(Chunk) Chunk
 }
