2804515b59f5686eafe69cc82f8f513ee9704247
Willa Walthall <ghthor@gmail.com>
2015-01-26 05:26:40 -0500

    go generate string values for the 4 quadrant constants

--- coord/quadrant.go
+++ coord/quadrant.go
@@ -1,15 +1,12 @@
+//go:generate stringer -type=Quad -output=quadrant_string.go
+
 package coord
 
+type Quad int
+
 const (
-	QUAD_NW = iota
+	QUAD_NW Quad = iota
 	QUAD_NE
 	QUAD_SE
 	QUAD_SW
-)
-
-const (
-	NW = iota
-	NE
-	SE
-	SW
 )
