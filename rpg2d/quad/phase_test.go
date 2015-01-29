a75cf02d6b4a6456863b89281f1a71ad4f89d411
Willa Walthall <ghthor@gmail.com>
2015-01-29 13:42:22 -0500

    Improve input phase impl coverage to 100%

--- rpg2d/quad/phase_test.go
+++ rpg2d/quad/phase_test.go
@@ -11,14 +11,13 @@ import (
 )
 
 func DescribePhase(c gospec.Context) {
-	q, err := quad.New(coord.Bounds{
-		TopL: coord.Cell{-16, 16},
-		BotR: coord.Cell{15, -15},
-	}, 4, nil)
-	c.Assume(err, IsNil)
-
 	c.Specify("the input phase", func() {
 		c.Specify("will remove any entites that move out of bounds", func() {
+			q, err := quad.New(coord.Bounds{
+				TopL: coord.Cell{-16, 16},
+				BotR: coord.Cell{15, -15},
+			}, 3, nil)
+			c.Assume(err, IsNil)
 
 			// A Single Entity
 			q = q.Insert(MockEntity{0, coord.Cell{-16, 16}})
@@ -41,25 +40,32 @@ func DescribePhase(c gospec.Context) {
 			q = q.Insert(MockEntity{1, coord.Cell{15, -15}})
 			q = q.Insert(MockEntity{2, coord.Cell{-1, 1}})
 			q = q.Insert(MockEntity{3, coord.Cell{0, 0}})
+			q = q.Insert(MockEntity{4, coord.Cell{5, -2}})
 
 			q, outOfBounds = quad.RunInputPhaseOn(q, quad.InputPhaseHandlerFn(func(chunk quad.Chunk, now stime.Time) quad.Chunk {
 				// Move the entity out of bounds
 				for i, e := range chunk.Entities {
-					if e.Id() == 1 {
+					switch e.Id() {
+					case 1:
+						// Move out of quadtree's bounds
 						chunk.Entities[i] = MockEntity{1, coord.Cell{16, -15}}
+					case 4:
+						// Move from SE to NE quadrant
+						chunk.Entities[i] = MockEntity{4, coord.Cell{5, 5}}
 					}
 				}
 
 				return chunk
 			}), stime.Time(0))
 
-			c.Expect(len(q.QueryBounds(q.Bounds())), Equals, 3)
+			c.Expect(len(q.QueryBounds(q.Bounds())), Equals, 4)
 			c.Expect(len(outOfBounds), Equals, 1)
 
 			c.Expect(q.QueryCell(coord.Cell{-16, 16})[0].Id(), Equals, int64(0))
 			c.Expect(outOfBounds[0].Id(), Equals, int64(1))
 			c.Expect(q.QueryCell(coord.Cell{-1, 1})[0].Id(), Equals, int64(2))
 			c.Expect(q.QueryCell(coord.Cell{0, 0})[0].Id(), Equals, int64(3))
+			c.Expect(q.QueryCell(coord.Cell{5, 5})[0].Id(), Equals, int64(4))
 
 		})
 	})
