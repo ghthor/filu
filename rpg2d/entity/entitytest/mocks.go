88aefc987d012097cdecff94d48bea5570955f58
Willa Walthall <ghthor@gmail.com>
2015-02-12 10:59:55 -0500

    Export mock entities internal fields

--- rpg2d/entity/entitytest/mocks.go
+++ rpg2d/entity/entitytest/mocks.go
@@ -9,27 +9,27 @@ import (
 
 type (
 	MockEntity struct {
-		id   int64
-		cell coord.Cell
+		EntityId   int64
+		EntityCell coord.Cell
 	}
 
 	MockEntityWithBounds struct {
-		id     int64
-		cell   coord.Cell
-		bounds coord.Bounds
+		EntityId     int64
+		EntityCell   coord.Cell
+		EntityBounds coord.Bounds
 	}
 )
 
 func (e MockEntity) String() string                    { return fmt.Sprintf("MockEntity%v", e.Id()) }
-func (e MockEntity) Id() int64                         { return e.id }
-func (e MockEntity) Cell() coord.Cell                  { return e.cell }
-func (e MockEntity) Bounds() coord.Bounds              { return coord.Bounds{e.cell, e.cell} }
+func (e MockEntity) Id() int64                         { return e.EntityId }
+func (e MockEntity) Cell() coord.Cell                  { return e.EntityCell }
+func (e MockEntity) Bounds() coord.Bounds              { return coord.Bounds{e.EntityCell, e.EntityCell} }
 func (e MockEntity) ToState() entity.State             { return e }
 func (e MockEntity) IsDifferentFrom(entity.State) bool { return true }
 
 func (e MockEntityWithBounds) String() string                    { return fmt.Sprintf("MockEntityWithBounds%v", e.Id()) }
-func (e MockEntityWithBounds) Id() int64                         { return e.id }
-func (e MockEntityWithBounds) Cell() coord.Cell                  { return e.cell }
-func (e MockEntityWithBounds) Bounds() coord.Bounds              { return e.bounds }
+func (e MockEntityWithBounds) Id() int64                         { return e.EntityId }
+func (e MockEntityWithBounds) Cell() coord.Cell                  { return e.EntityCell }
+func (e MockEntityWithBounds) Bounds() coord.Bounds              { return e.EntityBounds }
 func (e MockEntityWithBounds) ToState() entity.State             { return e }
 func (e MockEntityWithBounds) IsDifferentFrom(entity.State) bool { return true }
