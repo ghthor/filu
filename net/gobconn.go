6c61bea2f8b37976c825a6a0144c82a7451c7b43
Wilhelmina Drengwitz <ghthor@gmail.com>
2015-11-29 00:32:28 -0500

    [filu/net] Port Conn interface and GobConn impl from [aodd/game]
    [aodd] github.com/ghthor/aodd

---
+++ net/gobconn.go
@@ -0,0 +1,63 @@
+package net
+
+import (
+	"bufio"
+	"encoding/gob"
+	"io"
+)
+
+func init() {
+	types := []interface{}{
+		UserLoginRequest{},
+		UserLoginFailure{},
+		UserLoginSuccess{},
+		UserCreateSuccess{},
+	}
+
+	for _, t := range types {
+		gob.Register(t)
+	}
+}
+
+type gobConn struct {
+	enc  *gob.Encoder
+	wbuf *bufio.Writer
+
+	dec *gob.Decoder
+}
+
+func (c gobConn) Encode(ev EncodableType) error {
+	err := c.enc.Encode(ev.Type())
+	if err != nil {
+		return err
+	}
+
+	err = c.enc.Encode(ev)
+	if err != nil {
+		return err
+	}
+
+	return c.wbuf.Flush()
+}
+
+func (c gobConn) NextType() (t EncodedType, err error) {
+	err = c.dec.Decode(&t)
+	return
+}
+
+func (c gobConn) Decode(t EncodableType) error {
+	return c.dec.Decode(t)
+}
+
+func NewGobConn(rw io.ReadWriter) Conn {
+	wbuf := bufio.NewWriter(rw)
+	enc := gob.NewEncoder(wbuf)
+	dec := gob.NewDecoder(rw)
+
+	return gobConn{
+		enc:  enc,
+		wbuf: wbuf,
+
+		dec: dec,
+	}
+}
