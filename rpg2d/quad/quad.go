d09963d99d35be790deb2d04466a7929f07e7134
Wilhelmina Drengwitz <ghthor@gmail.com>
2016-08-28 19:55:57 -0400

    [filu/rpg2d/quad] fix comment grammar

--- rpg2d/quad/quad.go
+++ rpg2d/quad/quad.go
@@ -201,8 +201,7 @@ func (q quadLeaf) Bounds() coord.Bounds { return q.bounds }
 func (q quadLeaf) Insert(e entity.Entity) Quad {
 	// If the quad is full it must split
 	if len(q.entities) >= q.maxSize {
-		// Unless it has a width or height of 1
-		// Then is can't split
+		// Unless it has a width or height of 1 then it can't be split.
 		if q.Bounds().Height() > 1 && q.Bounds().Width() > 1 {
 			return q.divide().Insert(e)
 		}
